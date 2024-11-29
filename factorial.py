56a8f9ad46c67b458f38a1aed9c9b234f93e9c94
Polan9 <polanek26@gmail.com>
2024-11-29 18:47:11 +0100

    First commit lab3

---
+++ factorial.py
@@ -0,0 +1,13 @@
+
+def factorial(n):
+    list_of_numbers = [i for i in range(n + 1)]
+    result = []
+    for i in range(len(list_of_numbers)):
+        list_of_numbers.pop(0)
+        if len(list_of_numbers) > i + 1:
+            tmp = list_of_numbers[i] * list_of_numbers[i + 1]
+            list_of_numbers[i + 1] = tmp
+            list_of_numbers.append(tmp)
+            result.append(tmp)
+    return max(result)
+
---
+++ factorial_and_joblib.py
@@ -0,0 +1,19 @@
+from turtledemo.penrose import start
+
+from factorial import factorial
+from joblib import delayed, Parallel
+import time
+
+njobs_list = [2,5,10,15,25]
+
+for i in range(len(njobs_list)):
+    start = time.time()
+    a1 = Parallel(n_jobs=njobs_list[i])(delayed(factorial)(i) for i in range(3,10))
+    end = time.time()
+    time_taken = end - start
+    print(f"czas trwania obliczen dla njbos == {njobs_list[i]}: {time_taken:.3f}")
+
+
+
+test = factorial(5)
+print(test)
